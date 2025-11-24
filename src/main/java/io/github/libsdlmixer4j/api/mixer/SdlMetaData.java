ecbd39566a979e3a156ef03dd515d2c1c0235d9d
Apollo D. Sharpe, Sr. <1580939+adsharpe@users.noreply.github.com>
2025-11-24 17:59:02 -0600

    Meta data from music/sound file.

---
+++ src/main/java/io/github/libsdlmixer4j/api/mixer/SdlMetaData.java
@@ -0,0 +1,21 @@
+package io.github.libsdlmixer4j.api.mixer;
+
+import io.github.libsdlmixer4j.jna.SdlMixerNativeLibraryLoader;
+
+public class SdlMetaData {
+	
+	static {
+		SdlMixerNativeLibraryLoader.registerNativeMethods(SdlMetaData.class);
+	}
+	
+	public static native int Mix_GetMusicType(Mix_Music music);
+	public static native String Mix_GetMusicTitle(Mix_Music music);
+	public static native String Mix_GetMusicTitleTag(Mix_Music music);
+	public static native String Mix_GetMusicArtistTag(Mix_Music music);
+	public static native String Mix_GetMusicAlbumTag(Mix_Music music);
+	public static native String Mix_GetMusicCopyrightTag(Mix_Music music);
+	
+	
+	
+	private SdlMetaData() {}
+}
