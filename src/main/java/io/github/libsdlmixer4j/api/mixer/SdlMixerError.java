69cf227487bd1646b8bb5cbeb7897c59d195e3b1
Apollo D. Sharpe, Sr. <1580939+adsharpe@users.noreply.github.com>
2025-11-25 12:14:53 -0600

    Made methods public.

--- src/main/java/io/github/libsdlmixer4j/api/mixer/SdlMixerError.java
+++ src/main/java/io/github/libsdlmixer4j/api/mixer/SdlMixerError.java
@@ -5,22 +5,22 @@ import io.github.libsdl4j.api.error.SdlError;
 public class SdlMixerError {
 	
 	// Set last error message
-    int Mix_SetError(String fmt, Object... args) {
+    public int Mix_SetError(String fmt, Object... args) {
     	return SdlError.SDL_SetError(fmt, args);
     }
 
 	// Get last error message
-	String Mix_GetError() {
+    public String Mix_GetError() {
 		return SdlError.SDL_GetError();
 	}
 
 	// Clear last error message
-	void Mix_ClearError() {
+    public void Mix_ClearError() {
 		SdlError.SDL_ClearError();
 	}
 
 	// Set "out of memory" error
-	void Mix_OutOfMemory() {
+    public void Mix_OutOfMemory() {
 		SdlError.SDL_SetError("Out of memory");
 	}
 	
