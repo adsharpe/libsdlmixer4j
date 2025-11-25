571966a68a4f5e45a762213b6eb323058e6d01ed
Apollo D. Sharpe, Sr. <1580939+adsharpe@users.noreply.github.com>
2025-11-24 20:53:20 -0600

    Error convenience methods.

---
+++ src/main/java/io/github/libsdlmixer4j/api/mixer/SdlMixerError.java
@@ -0,0 +1,29 @@
+package io.github.libsdlmixer4j.api.mixer;
+
+import io.github.libsdl4j.api.error.SdlError;
+import io.github.libsdlmixer4j.jna.SdlMixerNativeLibraryLoader;
+
+public class SdlMixerError {
+	
+	// Set last error message
+    int Mix_SetError(String fmt, Object... args) {
+    	return SdlError.SDL_SetError(fmt, args);
+    }
+
+	// Get last error message
+	String Mix_GetError() {
+		return SdlError.SDL_GetError();
+	}
+
+	// Clear last error message
+	void Mix_ClearError() {
+		SdlError.SDL_ClearError();
+	}
+
+	// Set "out of memory" error
+	void Mix_OutOfMemory() {
+		SdlError.SDL_SetError("Out of memory");
+	}
+	
+	private SdlMixerError() {}
+}
